d555e3327a8d0f140c5d7fd0e600bd1462785df6
Jose Benito <jfbenitopa@gmail.com>
2016-03-03 18:10:41 +0100

    Deleting developer comments

--- orchextrasdk-data/src/main/java/gigigo/com/orchextra/data/datasources/db/beacons/BeaconsDBDataSourceImpl.java
+++ orchextrasdk-data/src/main/java/gigigo/com/orchextra/data/datasources/db/beacons/BeaconsDBDataSourceImpl.java
@@ -19,17 +19,97 @@
 package gigigo.com.orchextra.data.datasources.db.beacons;
 
 import android.content.Context;
+
 import com.gigigo.gggjavalib.business.model.BusinessError;
 import com.gigigo.gggjavalib.business.model.BusinessObject;
 import com.gigigo.ggglogger.GGGLogImpl;
 import com.gigigo.orchextra.dataprovision.proximity.datasource.BeaconsDBDataSource;
 import com.gigigo.orchextra.domain.model.entities.proximity.OrchextraBeacon;
 import com.gigigo.orchextra.domain.model.entities.proximity.OrchextraRegion;
-import gigigo.com.orchextra.data.datasources.db.RealmDefaultInstance;
-import io.realm.Realm;
+
 import java.util.List;
 
+import gigigo.com.orchextra.data.datasources.db.RealmDefaultInstance;
+import io.realm.Realm;
 
+public class BeaconsDBDataSourceImpl implements BeaconsDBDataSource {
+
+  private final Context context;
+  private final BeaconEventsUpdater beaconEventsUpdater;
+  private final BeaconEventsReader beaconEventsReader;
+  private final RealmDefaultInstance realmDefaultInstance;
+
+  public BeaconsDBDataSourceImpl(Context context, BeaconEventsUpdater beaconEventsUpdater,
+                                 BeaconEventsReader beaconEventsReader, RealmDefaultInstance realmDefaultInstance) {
+
+    this.context = context;
+    this.beaconEventsUpdater = beaconEventsUpdater;
+    this.beaconEventsReader = beaconEventsReader;
+    this.realmDefaultInstance = realmDefaultInstance;
+  }
+
+  @Override
+  public BusinessObject<OrchextraRegion> obtainRegionEvent(OrchextraRegion orchextraRegion) {
+    Realm realm = realmDefaultInstance.createRealmInstance(context);
+    try {
+      OrchextraRegion region = beaconEventsReader.obtainRegionEvent(realm, orchextraRegion);
+      return new BusinessObject<>(region, BusinessError.createOKInstance());
+    } catch (Exception e) {
+      return new BusinessObject<>(null, BusinessError.createKoInstance(e.getMessage()));
+    } finally {
+      if (realm != null) {
+        realm.close();
+      }
+    }
+  }
+
+  @Override
+  public BusinessObject<OrchextraRegion> storeRegionEvent(OrchextraRegion orchextraRegion) {
+    Realm realm = realmDefaultInstance.createRealmInstance(context);
+    try {
+      OrchextraRegion region = beaconEventsUpdater.storeRegionEvent(realm, orchextraRegion);
+      return new BusinessObject<>(region, BusinessError.createOKInstance());
+    } catch (Exception e) {
+      return new BusinessObject<>(null, BusinessError.createKoInstance(e.getMessage()));
+    } finally {
+      if (realm != null) {
+        realm.close();
+      }
+    }
+  }
+
+  @Override
+  public BusinessObject<OrchextraRegion> deleteRegionEvent(OrchextraRegion orchextraRegion) {
+    Realm realm = realmDefaultInstance.createRealmInstance(context);
+    try {
+      OrchextraRegion region = beaconEventsUpdater.deleteRegionEvent(realm, orchextraRegion);
+      return new BusinessObject<>(region, BusinessError.createOKInstance());
+    } catch (Exception e) {
+      return new BusinessObject<>(null, BusinessError.createKoInstance(e.getMessage()));
+    } finally {
+      if (realm != null) {
+        realm.close();
+      }
+    }
+  }
+
+  @Override public BusinessObject<OrchextraBeacon> storeBeaconEvent(OrchextraBeacon beacon) {
+    Realm realm = realmDefaultInstance.createRealmInstance(context);
+    try {
+      OrchextraBeacon storedBeacon = beaconEventsUpdater.storeBeaconEvent(realm, beacon);
+      return new BusinessObject<>(storedBeacon, BusinessError.createOKInstance());
+    } catch (Exception e) {
+      return new BusinessObject<>(null, BusinessError.createKoInstance(e.getMessage()));
+    } finally {
+      if (realm != null) {
+        realm.close();
+      }
+    }
+  }
+
+  /**
+   * @param requestTime
+   */
   @Override public void deleteAllBeaconsInListWithTimeStampt(int requestTime) {
     Realm realm = realmDefaultInstance.createRealmInstance(context);
     beaconEventsUpdater.deleteAllBeaconsInListWithTimeStampt(realm, requestTime);
--- orchextrasdk/src/main/java/com/gigigo/orchextra/device/bluetooth/beacons/ranging/BeaconRangingScannerImpl.java
+++ orchextrasdk/src/main/java/com/gigigo/orchextra/device/bluetooth/beacons/ranging/BeaconRangingScannerImpl.java
@@ -43,7 +43,190 @@ import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
 
+public class BeaconRangingScannerImpl implements RangeNotifier, BeaconRangingScanner{
 
+  private static BackgroundBeaconsRangingTimeType backgroundBeaconsRangingTimeType =
+          BackgroundBeaconsRangingTimeType.getType(BuildConfig.BACKGROUND_BEACONS_RANGING_TIME);
+
+  private final BeaconManager beaconManager;
+  private final BeaconsController beaconsController;
+  private final BeaconRegionAndroidMapper beaconRegionMapper;
+  private final BeaconAndroidMapper beaconAndroidMapper;
+
+  private HashMap<String, Thread> regionThreads = new HashMap<>();
+
+  private boolean ranging = false;
+
+  //avoid possible duplicates in region using set collection
+  private Set<Region> regions = (Set<Region>) Collections.synchronizedSet(new HashSet<Region>());
+
+  public BeaconRangingScannerImpl(BeaconManager beaconManager, BeaconsController beaconsController,
+                                  BeaconRegionAndroidMapper beaconRegionMapper, BeaconAndroidMapper beaconAndroidMapper) {
+    this.beaconManager = beaconManager;
+    this.beaconsController = beaconsController;
+    this.beaconRegionMapper = beaconRegionMapper;
+    this.beaconAndroidMapper = beaconAndroidMapper;
+    beaconManager.setRangeNotifier(this);
+  }
+
+  // region RangeNotifier Interface
+
+  /**
+   * Called from altbeacon Sdk when ranging is executed.
+   * @param collection beacons that are currently in range
+   * @param region region which all the scanned beacons received belongs to
+   */
+  @Override public void didRangeBeaconsInRegion(Collection<Beacon> collection, Region region) {
+    List beaconsList = new ArrayList<>(collection);
+    List<OrchextraBeacon> beacons = beaconAndroidMapper.externalClassListToModelList(beaconsList);
+    beaconsController.onBeaconsDetectedInRegion(beacons, beaconRegionMapper.externalClassToModel(
+            region));
+
+    if (collection.size() > 0) {
+      for (Beacon beacon : collection) {
+        GGGLogImpl.log("Beacon: " + beacon.getId1() + " major id:" + beacon.getId2()
+                + "  minor id: " + beacon.getId3());
+      }
+    }
+  }
+
+  //endregion
+
+  //region RegionsProvider Interface
+
+  /**
+   * This method will be called when regions are obtained from datasources and ready to be used
+   * @param regions regions to be scanned in ranging
+   */
+  @Override public void onRegionsReady(List<OrchextraRegion> regions) {
+    List<Region> altRegions = beaconRegionMapper.modelListToExternalClassList(regions);
+    this.regions.addAll(altRegions);
+    initRanging(BackgroundBeaconsRangingTimeType.INFINITE);
+  }
+
+  //endregion
+
+  //region BeaconRangingScanner Interface
+  //region init ranging
+  /**
+   * This method MUST be called in only in foreground, because calling this method in background
+   * could provoke an extremely drain of users battery.
+   *
+   * This method init ranging scan without previous monitoring and region detection over all
+   * existing regions. In addition this ranging scan will be enabled till the app changes mode
+   * from background to foreground or viceversa, so be aware of calling this method when app is in
+   * background.
+   *
+   * All existing regions for app will be monitored at the same time using this method.
+   *
+   * @param appRunningModeType current running mode of app AppRunningModeType.BACKGROUND or
+   * AppRunningModeType.FOREGROUND
+   *
+   * @throws BulkRangingScannInBackgroundException when called on from background
+   */
+  @Override public void initRangingScanForAllKnownRegions(AppRunningModeType appRunningModeType) {
+
+    if (appRunningModeType == AppRunningModeType.BACKGROUND){
+      throw new BulkRangingScannInBackgroundException("initRangingScanForAllKnownRegions "
+              + "MUST be called only if app is in foreground");
+    }
+
+    //callback argument into obtainRegionsToScan will call to onRegionsReady
+    beaconsController.getAllRegionsFromDataBase(this);
+  }
+
+
+  /**
+   * This method is called after the monitoring process discovers a Region, it saves the received
+   * region as a region to scan and calls ranging process to start during the specified seconds.
+   *
+   * The duration of the scan will depend on the AppRunningMode : FOREGROUND will scan indefinitely
+   * till the app went to background and BACKGROUND mode can scan during 10 seconds as default, 180
+   * seconds maximum or even not start the background ranging process.
+   * @See BackgroundBeaconsRangingTimeType
+   *
+   * End of scanning can be provoked as well when the scanned region exit event is received
+   *
+   * AppRunningModeType.FOREGROUND
+   * @param regions detected region
+   * @param backgroundBeaconsRangingTimeType ranging time for scan
+   *
+   * @throws BulkRangingScannInBackgroundException when called on from background
+   */
+  @Override public void initRangingScanForDetectedRegion(List<Region> regions,
+                                                         BackgroundBeaconsRangingTimeType backgroundBeaconsRangingTimeType) {
+
+    this.regions.addAll(regions);
+
+    initRanging(backgroundBeaconsRangingTimeType);
+  }
+
+  private void initRanging(BackgroundBeaconsRangingTimeType backgroundBeaconsRangingTimeType) {
+
+    manageGeneralBackgroundScanTimes(backgroundBeaconsRangingTimeType);
+
+    for (Region region:regions){
+
+      try {
+        manageRegionBackgroundScanTime(region, backgroundBeaconsRangingTimeType);
+        beaconManager.startRangingBeaconsInRegion(region);
+        ranging = true;
+      } catch (RemoteException e) {
+        e.printStackTrace();
+      }
+
+    }
+  }
+
+  private void manageGeneralBackgroundScanTimes(BackgroundBeaconsRangingTimeType time) {
+    if (time == BackgroundBeaconsRangingTimeType.MAX){
+      updateBackgroudScanTimes(BuildConfig.BACKGROUND_BEACONS_SCAN_TIME,
+              BuildConfig.BACKGROUND_BEACONS_BEETWEEN_SCAN_TIME);
+    }else{
+      updateBackgroudScanTimes(BeaconManager.DEFAULT_BACKGROUND_SCAN_PERIOD,
+              BeaconManager.DEFAULT_BACKGROUND_BETWEEN_SCAN_PERIOD);
+    }
+  }
+
+  private void manageRegionBackgroundScanTime(Region region,
+                                              BackgroundBeaconsRangingTimeType backgroundBeaconsRangingTimeType) {
+    if (backgroundBeaconsRangingTimeType != BackgroundBeaconsRangingTimeType.INFINITE){
+      scheduleEndOfRanging(region, backgroundBeaconsRangingTimeType.getIntValue());
+    }
+  }
+
+  private void scheduleEndOfRanging(final Region region, final int duration) {
+
+    Thread t = new Thread(new Runnable() {
+      @Override public void run() {
+        try {
+          Thread.sleep(duration);
+          stopRangingRegion(region);
+          regionThreads.remove(region.getUniqueId());
+        } catch (InterruptedException e) {
+          GGGLogImpl.log("This interruption coulbe be provoked see log below", LogLevel.WARN);
+        }
+      }
+    });
+    regionThreads.put(region.getUniqueId(), t);
+    t.start();
+  }
+
+  //endregion
+
+  //region Stop Ranging
+
+  /**
+   * This method will terminate the scan over all scanned regions at the moment.
+   */
+  @Override public void stopAllCurrentRangingScannedRegions() {
+    stopRangingAllRegions();
+  }
+
+  /**
+   * This method will terminate the scan over the specified region. This method should be called on
+   * monitoring exit event received over specific region.
+   */
   @Override public void stopRangingScanForDetectedRegion(Region region) {
     stopRangingRegion(region);
   }
