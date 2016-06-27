7ee95af96d7f5f9526493712137a1092a40061d2
Jose Benito <jfbenitopa@gmail.com>
2016-06-27 13:34:18 +0200

    Removed commented code in the MainActivity in app module

--- app/src/main/java/gigigo/com/orchextrasdk/MainActivity.java
+++ app/src/main/java/gigigo/com/orchextrasdk/MainActivity.java
@@ -39,16 +39,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         startOrchextra();
 
-//        stopOrchextra();
-
-
-//        new Handler().postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                setCrmUser();
-//            }
-//        }, 5000);
-
         //appoxeeSDK
         startAppoxee();
 
