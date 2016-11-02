b499612ec4a746394f2217ef63fe8416351910e0
Jose Benito <jfbenitopa@gmail.com>
2016-11-02 17:49:28 +0100

    Check style

--- orchextrasdk-dataprovision/src/main/java/com/gigigo/orchextra/dataprovision/config/model/strategy/CrmCustomFieldsSupported.java
+++ orchextrasdk-dataprovision/src/main/java/com/gigigo/orchextra/dataprovision/config/model/strategy/CrmCustomFieldsSupported.java
@@ -3,6 +3,6 @@ package com.gigigo.orchextra.dataprovision.config.model.strategy;
 import com.gigigo.orchextra.domain.model.MethodSupported;
 import com.gigigo.orchextra.domain.model.entities.tags.CrmCustomFields;
 
-public interface CrmCustomFieldsSupported extends MethodSupported{
-    CrmCustomFields getCrmCustomFields();
+public interface CrmCustomFieldsSupported extends MethodSupported {
+  CrmCustomFields getCrmCustomFields();
 }
