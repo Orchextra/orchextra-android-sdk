891a3302e6bde9015860c7c6d79cd86aca23899a
Manuel Rebollo <mrebollob@gmail.com>
2017-09-07 10:32:16 +0200

    Remove unused modules

--- orchextrasdk-data/src/main/java/gigigo/com/orchextra/data/datasources/db/OrchextraRealmModule.java
+++
@@ -1,7 +0,0 @@
-package gigigo.com.orchextra.data.datasources.db;
-
-import io.realm.annotations.RealmModule;
-
-@RealmModule(library = true, allClasses = true)
-public class OrchextraRealmModule {
-}
--- orchextrasdk-dataprovision/src/main/java/com/gigigo/orchextra/dataprovision/config/model/strategy/CrmCustomFieldsSupported.java
+++
@@ -1,8 +0,0 @@
-package com.gigigo.orchextra.dataprovision.config.model.strategy;
-
-import com.gigigo.orchextra.domain.model.MethodSupported;
-import com.gigigo.orchextra.domain.model.entities.tags.CrmCustomFields;
-
-public interface CrmCustomFieldsSupported extends MethodSupported {
-  CrmCustomFields getCrmCustomFields();
-}
