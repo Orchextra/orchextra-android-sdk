9e3262e29f5baacd00fc965fbaf9fdcb48f1659f
Manuel Rebollo <mrebollob@gmail.com>
2017-09-12 10:28:15 +0200

    Added user and device view

--- app/src/main/java/com/gigigo/orchextrasdk/demo/ui/settings/SettingsActivity.java
+++ app/src/main/java/com/gigigo/orchextrasdk/demo/ui/settings/SettingsActivity.java
@@ -25,6 +25,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -34,22 +35,25 @@ import com.gigigo.orchextra.core.Orchextra;
 import com.gigigo.orchextra.core.OrchextraErrorListener;
 import com.gigigo.orchextra.core.OrchextraStatusListener;
 import com.gigigo.orchextra.core.domain.entities.Error;
+import com.gigigo.orchextra.core.domain.entities.OxCRM;
+import com.gigigo.orchextra.core.domain.entities.OxDevice;
 import com.gigigo.orchextrasdk.demo.R;
 import com.gigigo.orchextrasdk.demo.ui.MainActivity;
 import com.gigigo.orchextrasdk.demo.ui.login.LoginActivity;
 import com.gigigo.orchextrasdk.demo.ui.login.ProjectData;
-import com.gigigo.orchextrasdk.demo.utils.CredentialsPreferenceManager;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class SettingsActivity extends AppCompatActivity implements SettingsView {
+public class SettingsActivity extends AppCompatActivity {
 
   private static final String TAG = "SettingsActivity";
-  private Orchextra orchextra;
   private EditText projectNameTextView;
   private EditText apiKeyTextView;
   private EditText apiSecretTextView;
   private Button finishButton;
-
-  private SettingsPresenter settingsPresenter;
+  Orchextra orchextra;
 
   public static void open(Context context) {
     Intent intent = new Intent(context, SettingsActivity.class);
@@ -60,23 +64,21 @@ public class SettingsActivity extends AppCompatActivity implements SettingsView 
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_settings);
 
-    settingsPresenter = new SettingsPresenter(this, new CredentialsPreferenceManager(this));
-
     projectNameTextView = (EditText) findViewById(R.id.project_name_editText);
     apiKeyTextView = (EditText) findViewById(R.id.apiKey_editText);
     apiSecretTextView = (EditText) findViewById(R.id.apiSecret_editText);
     finishButton = (Button) findViewById(R.id.finish_button);
 
+    initOrchextra();
     initView();
+    showData();
   }
 
-  @Override public void setupOrchextra() {
+  private void initOrchextra() {
     orchextra = Orchextra.INSTANCE;
     orchextra.setStatusListener(new OrchextraStatusListener() {
       @Override public void onStatusChange(boolean isReady) {
-        if (isReady) {
-          enableLogout();
-        } else {
+        if (!isReady) {
           MainActivity.open(SettingsActivity.this);
           finish();
         }
@@ -91,13 +93,26 @@ public class SettingsActivity extends AppCompatActivity implements SettingsView 
     });
   }
 
-  @Override public void finishOrchextra() {
-    orchextra.finish();
-  }
-
   private void initView() {
     initToolbar();
+    showProject();
+    initLogOutButton();
+  }
 
+  private void initLogOutButton() {
+    if (orchextra.isReady()) {
+      finishButton.setVisibility(View.VISIBLE);
+      finishButton.setOnClickListener(new View.OnClickListener() {
+        @Override public void onClick(View v) {
+          orchextra.finish();
+        }
+      });
+    } else {
+      finishButton.setVisibility(View.GONE);
+    }
+  }
+
+  private void showProject() {
     ProjectData projectData =
         ProjectData.getProjectDataByApiKey(ProjectData.getDefaultProjectDataList(), getApiKey());
 
@@ -110,29 +125,41 @@ public class SettingsActivity extends AppCompatActivity implements SettingsView 
       apiKeyTextView.setText(getApiKey());
       apiSecretTextView.setText("***");
     }
-
-    settingsPresenter.uiReady();
   }
 
-  @Override public void enableLogout() {
-    if (orchextra.isReady()) {
-      finishButton.setVisibility(View.VISIBLE);
-      finishButton.setOnClickListener(new View.OnClickListener() {
-        @Override public void onClick(View v) {
-          settingsPresenter.doLogout();
-        }
-      });
-    } else {
-      finishButton.setVisibility(View.GONE);
-    }
-  }
+  private void showData() {
 
-  @Override public void showProjectName(String projectName) {
+    List<String> tags = new ArrayList<>();
+    tags.add("color::green");
+    tags.add("color");
+    tags.add("56b0839435cb2741118b459f");
 
-  }
+    List<String> businessUnits = new ArrayList<>();
+    businessUnits.add("spain");
+    businessUnits.add("56b0839435cb2741118b459f");
 
-  @Override public void showProjectCredentials(String apiKey, String apiSecret) {
+    Map<String, String> customFields = new HashMap<>();
+    customFields.put("name", "Mike");
+    customFields.put("surname", "O'Brien");
+    customFields.put("hasGoldCard", "true");
 
+    OxCRM user = new OxCRM("f", "21-12-1984", tags, businessUnits, customFields);
+    OxDevice oxDevice = new OxDevice("", "", "", "", "", "", "", "", "", tags, businessUnits);
+
+    EditText genderEt = (EditText) findViewById(R.id.genderEt);
+    EditText birthDateEt = (EditText) findViewById(R.id.birthDateEt);
+    EditText tagsEt = (EditText) findViewById(R.id.tagsEt);
+    EditText businessUnitsEt = (EditText) findViewById(R.id.businessUnitsEt);
+    EditText deviceTagsEt = (EditText) findViewById(R.id.deviceTagsEt);
+    EditText deviceBusinessUnitsEt = (EditText) findViewById(R.id.deviceBusinessUnitsEt);
+
+    genderEt.setText(user.getGender());
+    birthDateEt.setText(user.getBirthDate());
+    tagsEt.setText(TextUtils.join(", ", user.getTags()));
+    businessUnitsEt.setText(TextUtils.join(", ", user.getBusinessUnits()));
+
+    deviceTagsEt.setText(TextUtils.join(", ", oxDevice.getTags()));
+    deviceBusinessUnitsEt.setText(TextUtils.join(", ", oxDevice.getBusinessUnits()));
   }
 
   private void initToolbar() {
