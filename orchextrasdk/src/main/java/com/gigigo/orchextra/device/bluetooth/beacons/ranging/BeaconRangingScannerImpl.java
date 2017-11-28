30c099d006f48be11c04dc75940dd7cde6e9c22c
Alberto Sainz Vegas <nuborisar@gmail.com>
2017-11-28 17:57:40 +0100

    Fix: 2 beacons exceptions fixed

--- orchextrasdk/src/main/java/com/gigigo/orchextra/device/bluetooth/beacons/ranging/BeaconRangingScannerImpl.java
+++ orchextrasdk/src/main/java/com/gigigo/orchextra/device/bluetooth/beacons/ranging/BeaconRangingScannerImpl.java
@@ -345,26 +345,33 @@ public class BeaconRangingScannerImpl implements RangeNotifier, BeaconRangingSca
 
     manageGeneralBackgroundScanTimes(backgroundBeaconsRangingTimeType);
 
-    new Thread(new Runnable() {
-      @Override public void run() {
-        for (Region region : regions) {
-          try {
-            //asv in the future, if we want more eddystone frame, we need to
-            //check if the region is eddystone region(getid1 length 20-22 chars)
-            //and create a null null null region, because, the eddytone frame URL/EID/IBeacon
-            //dont have ids like UID frame, enjoy!
+    try {
+      new Thread(new Runnable() {
+        @Override public void run() {
+          try{
+          for (Region region : regions) {
+            try {
+              //asv in the future, if we want more eddystone frame, we need to
+              //check if the region is eddystone region(getid1 length 20-22 chars)
+              //and create a null null null region, because, the eddytone frame URL/EID/IBeacon
+              //dont have ids like UID frame, enjoy!
 
-            //maybe wecan create scanner only forparse url eddystone, we need to check
-            manageRegionBackgroundScanTime(region, backgroundBeaconsRangingTimeType);
-            //asv hay q rescanear de region null para detectar urls de eddystone(se hace en el didenter)
-            beaconManager.startRangingBeaconsInRegion(region);
-            ranging = true;
-          } catch (RemoteException e) {
-            e.printStackTrace();
+              //maybe wecan create scanner only forparse url eddystone, we need to check
+              manageRegionBackgroundScanTime(region, backgroundBeaconsRangingTimeType);
+              //asv hay q rescanear de region null para detectar urls de eddystone(se hace en el didenter)
+              beaconManager.startRangingBeaconsInRegion(region);
+              ranging = true;
+            } catch (RemoteException e) {
+              e.printStackTrace();
+            }
+
+          }
+          } catch (Throwable throwable) {
+            //e.printStackTrace();
           }
         }
-      }
-    }).start();
+      }).start();
+    }catch (Throwable throwable){}
   }
 
   //asv ver con beni, lo del upgrade de tiempos troleaba ya q nunca conserbaba los valores x esto:
