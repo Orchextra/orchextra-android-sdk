638ffa095752d0ff5650d4761afcea9878ccb2cd
Alberto Sainz Vegas <nuborisar@gmail.com>
2018-02-21 15:35:32 +0100

    Fix: Apply solution for no context dialog

--- orchextrasdk/src/main/java/com/gigigo/orchextra/device/notifications/ForegroundNotificationBuilderImpl.java
+++ orchextrasdk/src/main/java/com/gigigo/orchextra/device/notifications/ForegroundNotificationBuilderImpl.java
@@ -32,62 +32,82 @@ import com.gigigo.orchextra.ui.dialogs.DialogTwoOptions;
 
 public class ForegroundNotificationBuilderImpl implements ForegroundNotificationBuilder {
 
-    private final ContextProvider contextProvider;
+  private final ContextProvider contextProvider;
 
-    private ActionDispatcherListener actionDispatcherListener;
-    private BasicAction action;
+  private ActionDispatcherListener actionDispatcherListener;
+  private BasicAction action;
 
-    public ForegroundNotificationBuilderImpl(ContextProvider contextProvider) {
-        this.contextProvider = contextProvider;
+  public ForegroundNotificationBuilderImpl(ContextProvider contextProvider) {
+    this.contextProvider = contextProvider;
+  }
+
+  @Override public void buildNotification(BasicAction action, OrchextraNotification notification) {
+    this.action = action;
+    notification.setShown(true); //This prevent show and show again the dialog notification
+    if (action.getActionType() == ActionType.NOTIFICATION) {
+      buildAcceptDialog(notification);
+    } else {
+      buildTwoOptionsDialog(notification);
     }
+  }
 
-    @Override
-    public void buildNotification(BasicAction action, OrchextraNotification notification) {
-        this.action = action;
-        notification.setShown(true); //This prevent show and show again the dialog notification
-        if (action.getActionType() == ActionType.NOTIFICATION) {
-            buildAcceptDialog(notification);
-        } else {
-            buildTwoOptionsDialog(notification);
+  private void waitForActivity() {
+    if (contextProvider.getCurrentActivity() != null && contextProvider.getCurrentActivity()
+        .isFinishing() || contextProvider.getCurrentActivity() == null) {
+      try {
+        Thread.sleep(500);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  private void buildAcceptDialog(OrchextraNotification notification) {
+    waitForActivity();
+    if (contextProvider.getCurrentActivity() != null) {
+      DialogOneOption dialog =
+          new DialogOneOption(contextProvider.getCurrentActivity(), notification.getTitle(),
+              notification.getBody(),
+              contextProvider.getCurrentActivity().getString(R.string.ox_accept_text),
+              positiveButtonListener);
+
+      dialog.onCreateDialog().show();
+    }
+  }
+
+  private void buildTwoOptionsDialog(OrchextraNotification notification) {
+    waitForActivity();
+    if (contextProvider.getCurrentActivity() != null) {
+      DialogTwoOptions dialog =
+          new DialogTwoOptions(contextProvider.getCurrentActivity(), notification.getTitle(),
+              notification.getBody(), contextProvider.getCurrentActivity().getString(R.string.ox_accept_text),
+              positiveButtonListener, contextProvider.getCurrentActivity().getString(R.string.ox_cancel_text),
+              negativeButtonListener);
+
+      dialog.onCreateDialog().show();
+    }
+  }
+
+  @Override
+  public void setActionDispatcherListener(ActionDispatcherListener actionDispatcherListener) {
+    this.actionDispatcherListener = actionDispatcherListener;
+  }
+
+  private DialogInterface.OnClickListener positiveButtonListener =
+      new DialogInterface.OnClickListener() {
+        @Override public void onClick(DialogInterface dialog, int which) {
+          if (actionDispatcherListener != null && action != null) {
+            actionDispatcherListener.onActionAccepted(action, true);
+          }
         }
-    }
+      };
 
-    private void buildAcceptDialog(OrchextraNotification notification) {
-        DialogOneOption dialog = new DialogOneOption(contextProvider.getCurrentActivity(), notification.getTitle(), notification.getBody(),
-                contextProvider.getCurrentActivity().getString(R.string.ox_accept_text), positiveButtonListener);
-
-        dialog.onCreateDialog().show();
-    }
-
-    private void buildTwoOptionsDialog(OrchextraNotification notification) {
-        DialogTwoOptions dialog = new DialogTwoOptions(contextProvider.getCurrentActivity(), notification.getTitle(), notification.getBody(),
-                contextProvider.getCurrentActivity().getString(R.string.ox_accept_text), positiveButtonListener,
-                contextProvider.getCurrentActivity().getString(R.string.ox_cancel_text), negativeButtonListener);
-
-        dialog.onCreateDialog().show();
-    }
-
-
-    @Override
-    public void setActionDispatcherListener(ActionDispatcherListener actionDispatcherListener) {
-        this.actionDispatcherListener = actionDispatcherListener;
-    }
-
-    private DialogInterface.OnClickListener positiveButtonListener = new DialogInterface.OnClickListener() {
-        @Override
-        public void onClick(DialogInterface dialog, int which) {
-            if (actionDispatcherListener != null && action != null) {
-                actionDispatcherListener.onActionAccepted(action, true);
-            }
+  private DialogInterface.OnClickListener negativeButtonListener =
+      new DialogInterface.OnClickListener() {
+        @Override public void onClick(DialogInterface dialog, int which) {
+          if (actionDispatcherListener != null && action != null) {
+            actionDispatcherListener.onActionDismissed(action, true);
+          }
         }
-    };
-
-    private DialogInterface.OnClickListener negativeButtonListener = new DialogInterface.OnClickListener() {
-        @Override
-        public void onClick(DialogInterface dialog, int which) {
-            if (actionDispatcherListener != null && action != null) {
-                actionDispatcherListener.onActionDismissed(action, true);
-            }
-        }
-    };
+      };
 }
